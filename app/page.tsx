35292ed726748b41050acd0f6b48e767bd12c7b7
BrayanD117 <brayandavid1928@gmail.com>
2024-08-26 16:02:05 -0500

    Start home cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import CarouselComponent from './components/Carousel'
 import { Container } from '@mantine/core'
+import HomeCards from './components/Home/HomeCards'
 
 const HomePage = () => {
   return (
     <>
       <CarouselComponent />
       <Container size={"xl"}>
-        <h1>HomePage</h1>
+        <HomeCards />
       </Container>
     </>
   )
