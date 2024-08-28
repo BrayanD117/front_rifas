981d96321aa40828af9fd2cf52df136aeace22a3
BrayanD117 <brayandavid1928@gmail.com>
2024-08-28 15:03:47 -0500

    Update UI in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,154 +1,114 @@
-"use client"
+"use client";
 
 import {
-    HoverCard,
-    Group,
-    Button,
-    UnstyledButton,
-    Text,
-    SimpleGrid,
-    ThemeIcon,
-    Anchor,
-    Divider,
-    Center,
-    Box,
-    Burger,
-    Drawer,
-    Collapse,
-    ScrollArea,
-    rem,
-    useMantineTheme,
-    Flex,
-  } from '@mantine/core';
-  import { useDisclosure } from '@mantine/hooks';
-  import {
-    IconNotification,
-    IconCode,
-    IconBook,
-    IconChartPie3,
-    IconFingerprint,
-    IconCoin,
-    IconChevronDown,
-    IconShoppingCartFilled,
-  } from '@tabler/icons-react';
-  import classes from './styles/Navbar.module.css';
-  
-  const mockdata = [
-    {
-      icon: IconCode,
-      title: 'Open source',
-      description: 'This Pokémon’s cry is very loud and distracting',
-    },
-    {
-      icon: IconCoin,
-      title: 'Free for everyone',
-      description: 'The fluid of Smeargle’s tail secretions changes',
-    },
-    {
-      icon: IconBook,
-      title: 'Documentation',
-      description: 'Yanma is capable of seeing 360 degrees without',
-    },
-    {
-      icon: IconFingerprint,
-      title: 'Security',
-      description: 'The shell’s rounded shape and the grooves on its.',
-    },
-    {
-      icon: IconChartPie3,
-      title: 'Analytics',
-      description: 'This Pokémon uses its flying ability to quickly chase',
-    },
-    {
-      icon: IconNotification,
-      title: 'Notifications',
-      description: 'Combusken battles with the intensely hot flames it spews',
-    },
-  ];
-  
-  export function Navbar() {
-    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-    const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-    const theme = useMantineTheme();
-  
-    const links = mockdata.map((item) => (
-      <UnstyledButton className={classes.subLink} key={item.title}>
-        <Group wrap="nowrap" align="flex-start">
-          <ThemeIcon size={34} variant="default" radius="md">
-            <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-          </ThemeIcon>
-          <div>
-            <Text size="sm" fw={500}>
-              {item.title}
-            </Text>
-            <Text size="xs" c="dimmed">
-              {item.description}
-            </Text>
-          </div>
-        </Group>
-      </UnstyledButton>
-    ));
-  
-    return (
-      <Box>
-        <header className={classes.header}>
-          <Group className={classes.loginButton} justify="flex-end">
-            <Flex visibleFrom="sm"  mih={50}
+  Group,
+  Button,
+  Divider,
+  Box,
+  Burger,
+  Drawer,
+  ScrollArea,
+  rem,
+  useMantineTheme,
+  Image,
+  Flex,
+  Center,
+  Grid,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconShoppingCartFilled } from "@tabler/icons-react";
+import classes from "./styles/Navbar.module.css";
+
+export function Navbar() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const theme = useMantineTheme();
+
+  return (
+    <Box>
+      <header className={classes.header}>
+        <Grid justify="space-between" align="center">
+          <Grid.Col span={4}>
+            <Group justify="flex-start">
+              <Center>
+                <Image
+                  mt={-20}
+                  alt="Rifa Mania"
+                  h={80}
+                  src="/assets/Logo-Rifa-Manía.webp"
+                />
+              </Center>
+            </Group>
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Flex
+              visibleFrom="sm"
+              mt={-20}
+              mih={50}
               gap="md"
-              justify="flex-end"
-              align="flex-start"
+              justify="center"
+              align="center"
               direction="row"
-              wrap="wrap">
+              wrap="wrap"
+            >
               <Button variant="default">Iniciar sesion</Button>
             </Flex>
-            <Group>
-            <Button
-              h={100}
-              color="green"
-              className={classes.buttonHover}
-              styles={{
-                root: {
-                  right: '-20px',
-                  bottom: '0px',
-                  height: '80px',
-                  width: '100px',
-                  borderBottomLeftRadius: '40px',
-                  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  zIndex: 1001,
-                },
-              }}
-            >
-              <IconShoppingCartFilled 
-                className={classes.cartIcon}
-                style={{ width: rem(45), height: rem(45) }}
-                stroke={1.5}
+            <Burger
+              opened={drawerOpened}
+              onClick={toggleDrawer}
+              hiddenFrom="sm"
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Group justify="flex-end">
+              <Button
+                h={110}
+                mt={-10}
+                color="green"
+                className={classes.buttonHover}
+                styles={{
+                  root: {
+                    right: "-20px",
+                    bottom: "0px",
+                    height: "80px",
+                    width: "100px",
+                    borderBottomLeftRadius: "40px",
+                    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    zIndex: 1001,
+                  },
+                }}
+              >
+                <IconShoppingCartFilled
+                  className={classes.cartIcon}
+                  style={{ width: rem(45), height: rem(45) }}
+                  stroke={1.5}
                 />
-            </Button>
+              </Button>
             </Group>
-            <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          </Grid.Col>
+        </Grid>
+      </header>
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Rifas"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+          <Divider my="sm" />
+          <Group justify="center" grow pb="xl" px="md">
+            <Button variant="default">Iniciar sesion</Button>
+            <Button>Registrarme</Button>
           </Group>
-
-        </header>
-        <Drawer
-          opened={drawerOpened}
-          onClose={closeDrawer}
-          size="100%"
-          padding="md"
-          title="Rifas"
-          hiddenFrom="sm"
-          zIndex={1000000}
-        >
-          <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
-            <Divider my="sm" />
-            <Group justify="center" grow pb="xl" px="md">
-              <Button variant="default">Iniciar sesion</Button>
-              <Button>Registrarme</Button>
-            </Group>
-          </ScrollArea>
-        </Drawer>
-      </Box>
-    );
-  }
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+}
