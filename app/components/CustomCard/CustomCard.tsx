41996906df750081a77bda18b87d984eda3e9481
juancai0104 <2220201024@estudiantesunibague.edu.co>
2024-08-28 17:03:39 -0500

    Update card props

--- app/components/CustomCard/CustomCard.tsx
+++ app/components/CustomCard/CustomCard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group, Grid } from '@mantine/core';
 
 interface CustomCardProps {
     imageSrc: string;
@@ -10,35 +10,39 @@ interface CustomCardProps {
     badgeColor: string;
     description: string;
     buttonText: string;
-  }
+}
 
 const CustomCard: React.FC<CustomCardProps> = ({
     imageSrc, imageAlt, title, badgeText, badgeColor, description, buttonText
 }) => {
-  return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src={imageSrc}
-          height={160}
-          alt={imageAlt}
-        />
-      </Card.Section>
+    return (
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+                <Image
+                    src={imageSrc}
+                    height={160}
+                    alt={imageAlt}
+                />
+            </Card.Section>
 
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>{title}</Text>
-        <Badge color={badgeColor}>{badgeText}</Badge>
-      </Group>
+            <Grid mt={"md"} mb={"xs"}>
+                <Grid.Col span={8} >
+                    <Text fw={500}>{title}</Text>
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <Badge color={badgeColor}>{badgeText}</Badge>
+                </Grid.Col>
+            </Grid>
 
-      <Text size="sm" c="dimmed">
-        {description}
-      </Text>
+            <Text size="sm" c="dimmed">
+                {description}
+            </Text>
 
-      <Button color="blue" fullWidth mt="md" radius="md">
-        {buttonText}
-      </Button>
-    </Card>
-  );
+            <Button color="blue" fullWidth mt="md" radius="md">
+                {buttonText}
+            </Button>
+        </Card>
+    );
 }
 
 export default CustomCard;
--- app/components/Home/HomeCards.tsx
+++ app/components/Home/HomeCards.tsx
@@ -8,7 +8,18 @@ const HomeCards: React.FC = () => {
     <>
       <Container mt={"md"} size={"xl"}>
         <Grid>
-          <Grid.Col span={3}>
+          <Grid.Col span={{ base: 12, xs: 6, md: 4, lg: 3 }}>
+            <CustomCard
+              imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              imageAlt="Norway"
+              title="Hola"
+              badgeText="On Sale"
+              badgeColor="pink"
+              description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
+              buttonText="Book classic tour now"
+            />
+          </Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 6, md: 4, lg: 3 }}>
             <CustomCard
               imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
               imageAlt="Norway"
@@ -19,7 +30,7 @@ const HomeCards: React.FC = () => {
               buttonText="Book classic tour now"
             />
           </Grid.Col>
-          <Grid.Col span={3}>
+          <Grid.Col span={{ base: 12, xs: 6, md: 4, lg: 3 }}>
             <CustomCard
               imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
               imageAlt="Norway"
@@ -30,18 +41,7 @@ const HomeCards: React.FC = () => {
               buttonText="Book classic tour now"
             />
           </Grid.Col>
-          <Grid.Col span={3}>
-            <CustomCard
-              imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-              imageAlt="Norway"
-              title="Norway Fjord Adventures"
-              badgeText="On Sale"
-              badgeColor="pink"
-              description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
-              buttonText="Book classic tour now"
-            />
-          </Grid.Col>
-          <Grid.Col span={3}>
+          <Grid.Col span={{ base: 12, xs: 6, md: 4, lg: 3 }}>
             <CustomCard
               imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
               imageAlt="Norway"
