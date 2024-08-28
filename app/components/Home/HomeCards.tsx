3ff620e6cfc46ebbb92a76167073e5cb83ea1aeb
juancai0104 <2220201024@estudiantesunibague.edu.co>
2024-08-28 16:43:26 -0500

    Add cards in grid to home

--- app/components/Home/HomeCards.tsx
+++ app/components/Home/HomeCards.tsx
@@ -1,18 +1,60 @@
+"use client"
+
+import { Container, Grid } from "@mantine/core";
 import CustomCard from "../CustomCard/CustomCard";
 
 const HomeCards: React.FC = () => {
   return (
     <>
-      <div>HomeCards</div>
-      <CustomCard
-          imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-          imageAlt="Norway"
-          title="Norway Fjord Adventures"
-          badgeText="On Sale"
-          badgeColor="pink"
-          description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
-          buttonText="Book classic tour now"
-        />
+      <Container mt={"md"} size={"xl"}>
+        <Grid>
+          <Grid.Col span={3}>
+            <CustomCard
+              imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              imageAlt="Norway"
+              title="Norway Fjord Adventures"
+              badgeText="On Sale"
+              badgeColor="pink"
+              description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
+              buttonText="Book classic tour now"
+            />
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <CustomCard
+              imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              imageAlt="Norway"
+              title="Norway Fjord Adventures"
+              badgeText="On Sale"
+              badgeColor="pink"
+              description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
+              buttonText="Book classic tour now"
+            />
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <CustomCard
+              imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              imageAlt="Norway"
+              title="Norway Fjord Adventures"
+              badgeText="On Sale"
+              badgeColor="pink"
+              description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
+              buttonText="Book classic tour now"
+            />
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <CustomCard
+              imageSrc="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              imageAlt="Norway"
+              title="Norway Fjord Adventures"
+              badgeText="On Sale"
+              badgeColor="pink"
+              description="With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway"
+              buttonText="Book classic tour now"
+            />
+          </Grid.Col>
+        </Grid>
+      </Container>
+      
     </>
   )
 }
