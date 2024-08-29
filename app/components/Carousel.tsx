188b6a29e333b6e822d8aee7b0928800a1c07947
BrayanD117 <brayandavid1928@gmail.com>
2024-08-29 08:07:08 -0500

    Add autoplay to carosuel

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,8 +2,10 @@
 
 import '@mantine/carousel/styles.css';
 import { Image } from '@mantine/core';
+import Autoplay from 'embla-carousel-autoplay';
 import { Carousel } from '@mantine/carousel';
 import classes from './styles/Carousel.module.css';
+import { useRef } from 'react';
 
 const images = [
     'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-1.png',
@@ -14,13 +16,14 @@ const images = [
   ];
 
 const CarouselComponent = () => {
-const slides = images.map((url) => (
+  const autoplay = useRef(Autoplay({ delay: 4000 }));
+  const slides = images.map((url) => (
     <Carousel.Slide key={url}>
         <Image src={url} />
     </Carousel.Slide>
     ));
   return (
-    <Carousel withIndicators height={250} loop classNames={classes}>
+    <Carousel withIndicators height={350} loop classNames={classes} plugins={[autoplay.current]}>
         {slides}
     </Carousel>
   )
