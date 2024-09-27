d5673f26ad23ad3a922bf2c0cbb3f0a6add655ce
BrayanD117 <brayandavid1928@gmail.com>
2024-09-27 09:23:44 -0500

    Add dropzone styles in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import '@mantine/notifications/styles.css';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
+import '@mantine/dropzone/styles.css';
 import { Notifications } from '@mantine/notifications';
 import { AuthProvider } from './context/AuthContext';
 
